372a1d651ea1e3a939052cf34bfdb50004b8e507
Trent <trenton.mason14@outlook.com>
2021-03-12 17:53:01 -0500

    cleaned up main(), cant move cursor off screen

--- editor.c
+++ editor.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <curses.h>
+#include <sys/ioctl.h>
  
 #define CTRL_KEY(x) ((x) & 0x1f)
 #define CURSOR "\u2588"
@@ -14,6 +15,8 @@ struct filebuf {
 };
 
 struct visualbuf {
+	int maxx;
+	int maxy;
 	int curx;
 	int cury;
 	int linelen;
@@ -22,57 +25,119 @@ struct visualbuf {
 	char *newline; // new line with tabs replaced with spaces
 };
 
-void write_buf_to_file(struct filebuf buf);
-void write_file_rows(struct filebuf buf);
-void read_file_to_buf(struct filebuf buf);
-void update_screen(struct visualbuf buf);
-char read_input(char input);
+void write_buf_to_file(int len, char *filename, char *buf);
+void write_file_rows();
+void read_file_to_buf(struct filebuf *fbuf, char *filename);
+void update_screen();
+void process_input(char input);
+int getsize(char *filename);
+
+
+// struct filebuf _fbuf;
+struct visualbuf _vbuf;
+struct visualbuf *vbuf = &_vbuf;
 
 int main(int argc, char *argv[]) {
-	struct visualbuf _vbuf;
-	struct visualbuf *vbuf = &_vbuf;
+	char input; // input buffer
+	struct filebuf _fbuf;
+	struct filebuf *fbuf = &_fbuf;
+	fbuf->len = getsize("test.txt");
 	vbuf->curx = 0;
 	vbuf->cury = 0;
-	int row, col;
-	char input; // input buffer
 	initscr();
+	getmaxyx(stdscr, vbuf->maxy,vbuf->maxx);
 	noecho();
-	getmaxyx(stdscr, row, col);
 	cbreak();
+	read_file_to_buf(fbuf, "test.txt");
+	update_screen(fbuf);
 	move(0,0);
 	while (1) {
 		input = getch();
-		switch(input) {
-			case CTRL_KEY('c'):
-				exit(0);
-			case '\033':
-				getch();
-				switch(getch()){
-					case 65:
-						vbuf->cury--;
-						move(vbuf->cury, vbuf->curx);
-						break;
-					case 66:
-						vbuf->cury++;
-						move(vbuf->cury, vbuf->curx);
-						break;
-					case 67:
-						vbuf->curx++;
-						move(vbuf->cury, vbuf->curx);
-						break;
-					case 68:
-						vbuf->curx--;
-						move(vbuf->cury, vbuf->curx);
-						break;
-				}
-				break;
-		}
+		process_input(input);
 		refresh();
 	}
 	endwin();
 	return 0;
 }
 
+void read_file_to_buf(struct filebuf *fbuf, char *filename) {
+	FILE *file = fopen(filename, "r");
+	fbuf->data = malloc(sizeof(char)*fbuf->len);
+	fread(fbuf->data, fbuf->len, 1, file);
+	fclose(file);
+}
 
+void update_screen(struct filebuf *fbuf) {
+	printw(fbuf->data);
+}
 
-// https://en.cppreference.com/w/c/io/setvbuf
+void process_input(char input) {
+	switch(input) {
+		case CTRL_KEY('c'):
+			exit(0);
+		case '\033':
+			getch();
+			switch(getch()){
+				case 65:
+					if(vbuf->cury < 0) {
+						vbuf->cury = 0;
+					}
+					else{
+						vbuf->cury--;
+					}
+					move(vbuf->cury, vbuf->curx);
+					break;
+				case 66:
+					if(vbuf->cury > vbuf->maxy){
+						vbuf->cury = vbuf->maxy;
+					}
+					else {
+						vbuf->cury++;
+					}
+					move(vbuf->cury, vbuf->curx);
+					break;
+				case 67:
+					if(vbuf->curx > vbuf->maxx){
+						vbuf->curx = vbuf->maxx;
+					}
+					else {
+						vbuf->curx++;
+					}
+					move(vbuf->cury, vbuf->curx);
+					break;
+				case 68:
+					if(vbuf->curx < 0) {
+						vbuf->curx = 0;
+					}
+					else{
+						vbuf->curx--;
+					}
+					move(vbuf->cury, vbuf->curx);
+					break;
+			}
+			break;
+	}
+}
+
+int getsize(char *filename){ // counts each byte of the file it opens
+	FILE *file = fopen(filename, "r");
+	if (file == NULL){
+		printf("ERROR: file failed to open..\n");
+		exit(1);
+	}
+	int count = 0;
+	char c;
+	if (file) {
+		while(1) {
+			c = fgetc(file); // read byte and store it in c
+			if (c == EOF){ // if the byte is equal to the end of file byte 
+				break; // then exit the loop and continue through the function
+			}
+			else{
+				count++; // increment the count 
+			}
+		}
+	}
+	fclose(file);
+	return count; // return count
+}
